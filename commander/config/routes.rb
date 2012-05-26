c03f6f13d7df963371e992d6071e0ca8f12119ed
grimm <jason@gina.alaska.edu>
2012-05-25 17:51:52 -0800

    Adding new bootstrap-sass layout for commander interface

--- commander/config/routes.rb
+++ commander/config/routes.rb
@@ -1,5 +1,7 @@
 Commander::Application.routes.draw do
   
+  get "dashboard/index"
+
   namespace :sensors do
     resources :platforms do 
       resources :sensors
@@ -56,7 +58,7 @@ Commander::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'dashboard#index'
 
   # See how all your routes lay out with "rake routes"
 

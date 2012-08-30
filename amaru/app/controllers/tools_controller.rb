f79fa826ad10610e5a6263c31a3e13d7a05fda16
grimm <jason@gina.alaska.edu>
2012-08-30 11:52:18 -0800

    Group platforms by sensors is complete and working.

--- amaru/app/controllers/tools_controller.rb
+++ amaru/app/controllers/tools_controller.rb
@@ -5,7 +5,7 @@ class ToolsController < ApplicationController
       sensors << platform.sensors.collect(&:source_field)
     end
     @all_sensors = sensors.flatten.uniq
-    @all_groups = Group.asc(:name)
+    @all_groups = Group.asc(:name).collect(&:name)
 
     respond_to do |format|
       format.html
@@ -16,5 +16,15 @@ class ToolsController < ApplicationController
     group = params["group"]
     @group = Group.where(name: group).first || Group.new unless group.nil?
     @group.update_attributes!( name: group ) if @group.new_record?
+
+    params["sensors"].each do |sensor|
+      platforms = Platform.where("sensors.source_field" => sensor)
+      @group.platforms << platforms
+    end
+
+    respond_to do |format|
+      format.html { redirect_to tools_path, notice: "Platforms were successfully grouped into group <i>#{group}</i>." }
+      format.json { head :no_content }
+    end
   end
 end

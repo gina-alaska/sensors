db2e3a4af0007a6ad1ddbdb30e1b693b2a9a31ce
grimm <jason@gina.alaska.edu>
2012-08-31 14:13:37 -0800

    Added status counts to dashboard page.

--- amaru/app/controllers/dashboard_controller.rb
+++ amaru/app/controllers/dashboard_controller.rb
@@ -2,6 +2,11 @@
 class DashboardController < ApplicationController
   def index
     @status = Statu.latest(12)
+    @num_groups = Group.count
+    @num_platforms = Platform.count
+    @num_events = Event.count
+    @num_graphs = Graph.count
+    @num_alerts = Alert.count
 
   	@failures = Resque::Failure.all(0, Resque::Failure.count)
   	if @failures.is_a? Hash

fc56d3bd1eb0b5900e79b17da74f420c69ffe4c9
grimm <jason@gina.alaska.edu>
2012-09-14 11:21:09 -0800

    Improved the set_current javascript to better handle which page the user ends up on when changing organizations.

--- amaru/app/views/organizations/set_current.js
+++ amaru/app/views/organizations/set_current.js
@@ -1,2 +1,15 @@
-console.log(top.location)
-top.location.reload(true);
+var pathname = top.location.pathname;
+var newlocation = "/dashboard";
+if (/platform/.test(pathname)){
+  newlocation = "/platforms";
+}
+if (/groups/.test(pathname)){
+  newlocation = "/groups";
+}
+if (/organization/.test(pathname)){
+  newlocation = "/organizations";
+}
+if (/tools/.test(pathname)){
+  newlocation = "/tools";
+}
+top.location.replace(newlocation);

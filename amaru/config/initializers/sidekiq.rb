52361bda3a370ecc2f9ff8c73910cf2b9f45a1da
grimm <jason@gina.alaska.edu>
2012-09-26 14:41:11 -0800

    Sidekiq Redis initializer.

---
+++ amaru/config/initializers/sidekiq.rb
@@ -0,0 +1,7 @@
+Sidekiq.configure_server do |config|
+  config.redis = { :url => 'redis://hermes.gina.alaska.edu:6379/12', :namespace => 'amaru' }
+end
+
+# Sidekiq.configure_client do |config|
+#   config.redis = { :url => 'redis://beta.gina.alaska.edu:6379/12', :namespace => 'amaru', :size => 1 }
+# end

8b148b8280990d82c53d165618af045b629884be
grimm <jason@gina.alaska.edu>
2013-05-10 10:06:05 -0800

    converted commands to the new re factored data storage.

--- amaru/app/models/command.rb
+++ amaru/app/models/command.rb
@@ -6,7 +6,9 @@ class Command
 	field :ends_at,       type: DateTime
 	field :command,       type: String
 	field :window,        type: String
-        field :command_text,  type: String
+  field :command_text,  type: String
+
+  has_many :output_data, class_name: "Datum"
 
   default_scope asc(:index)
 	embedded_in :event

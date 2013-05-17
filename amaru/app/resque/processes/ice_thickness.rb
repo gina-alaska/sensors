31a60e2a3072e2e6ab8c1b36ab89c6bf8153cb05
grimm <jason@gina.alaska.edu>
2013-05-17 11:36:39 -0800

    Fixed ice thickness command so it handles snow depth correctly.

--- amaru/app/resque/processes/ice_thickness.rb
+++ amaru/app/resque/processes/ice_thickness.rb
@@ -10,7 +10,7 @@ module Processes
 
     snow_data = opts[:processed_data].send(snow_depth.to_sym)
     if snow_data == @platform.no_data_value or snow_data.nil?
-      snow_data = 0
+      snow_data = 0.0
     end
 
     data = input.shift
@@ -18,7 +18,6 @@ module Processes
       result = data
     else
       ss_correction = sound_speed(opts[:data_row].send(water_sensor.to_sym))
-      snow_data = @group.processed_data.where(capture_date: capture_date).first[snow_depth.to_sym]
 
       if snow_data.to_f < 0.0
         result = sensor_height + (ss_correction * data.to_f) + snow_data.to_f

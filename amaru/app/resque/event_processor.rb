ae643922175476c5d45c303a0929f57a6bea4401
grimm <jason@gina.alaska.edu>
2014-03-19 11:34:16 -0800

    Skip processing and go strait to filtering if no event commands.

--- amaru/app/resque/event_processor.rb
+++ amaru/app/resque/event_processor.rb
@@ -17,32 +17,34 @@ class EventProcessor
 
       puts "Started process event #{event.name} for #{platform.name}"
 
-      platform.raw_data.no_timeout.batch_size(1000).each do |data_row|
-        output = nil
+      unless processes = event.commands    # Get all commands from this event
+        platform.raw_data.no_timeout.batch_size(1000).each do |data_row|
+          output = nil
 
-        # Assemble needed raw data fields
-        data = []
-        event.from.each do |field|
-          data << data_row.send(field) unless field.nil?
+          # Assemble needed raw data fields
+          data = []
+          event.from.each do |field|
+            data << data_row.send(field) unless field.nil?
+          end
+
+          processed_data = group.processed_data.where(capture_date: data_row.capture_date).first
+          if processed_data.nil?
+            processed_data = group.processed_data.build(capture_date: data_row.capture_date)
+          end
+
+          processor = ProcessorCommands.new(group, platform, event)
+          processes.each do |cmd|
+            start_time = cmd.starts_at.nil? ? data_row.capture_date : cmd.starts_at
+            end_time = cmd.ends_at.nil? ? data_row.capture_date : cmd.ends_at
+            next unless data_row.capture_date.between?(start_time, end_time)
+
+            data = processor.send(cmd.command.downcase.to_sym, { cmd: cmd, input: data, data_row: data_row, processed_data: processed_data })
+          end
+
+          processed_data.update_attribute(event.name.to_sym, data.shift)
         end
-
-        processed_data = group.processed_data.where(capture_date: data_row.capture_date).first
-        if processed_data.nil?
-          processed_data = group.processed_data.build(capture_date: data_row.capture_date)
-        end
-
-        processor = ProcessorCommands.new(group, platform, event)
-        processes = event.commands    # Get all commands from this event
-        processes.each do |cmd|
-          start_time = cmd.starts_at.nil? ? data_row.capture_date : cmd.starts_at
-          end_time = cmd.ends_at.nil? ? data_row.capture_date : cmd.ends_at
-          next unless data_row.capture_date.between?(start_time, end_time)
-
-          data = processor.send(cmd.command.downcase.to_sym, { cmd: cmd, input: data, data_row: data_row, processed_data: processed_data })
-        end
-
-        processed_data.update_attribute(event.name.to_sym, data.shift)
       end
+      puts "end processing"
 
       # Do filters if there are any
       unless event.filter == ""

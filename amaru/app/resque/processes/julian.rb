175cdc16c9804dada280e6d3fbfd3f3d6a3f6075
Grimm <jason@gina.alaska.edu>
2015-01-27 12:50:00 -0900

    Changed function so it will return a float.

--- amaru/app/resque/processes/julian.rb
+++ amaru/app/resque/processes/julian.rb
@@ -9,7 +9,7 @@ module Processes
 
     data = input.shift.to_i
 
-    result = data + (((opts[:data_row].send(hour_sensor.to_sym).to_i * 60) + opts[:data_row].send(minute_sensor.to_sym).to_i)/ 1440)
+    result = data + (((opts[:data_row].send(hour_sensor.to_sym).to_i * 60.0) + opts[:data_row].send(minute_sensor.to_sym).to_i)/ 1440.0)
 
     Array.wrap(result)
   end
